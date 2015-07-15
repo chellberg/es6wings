c7a94db569d5776722369847b535e075b8e92a4e
Corin Chellberg <corinchellberg@gmail.com>
2015-07-15 01:12:31 -0400

    extract handleSubmit function

--- bundle.js
+++ bundle.js
@@ -1,13 +1,15 @@
 'use strict';
 
 $(document).ready(function () {
-  $('form').on('submit', function (e) {
-    e.preventDefault();
-    var totalPrice = $('input#price').val();
-    var wingCount = $('input#count').val();
-    console.log('Price per wing: ' + pricePerWing(totalPrice, wingCount));
-  });
+  $('form').on('submit', handleSubmit);
 });
+
+var handleSubmit = function handleSubmit(e) {
+  e.preventDefault();
+  var totalPrice = $('input#price').val();
+  var wingCount = $('input#count').val();
+  console.log('Price per wing: ' + pricePerWing(totalPrice, wingCount));
+};
 
 var pricePerWing = function pricePerWing(total, count) {
   return +total / +count;
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-$( document ).ready(() => {
-  $('form').on('submit', e => {
+$( document ).ready( () => {
+  $('form').on('submit', handleSubmit)
+})
+
+const handleSubmit = e => {
     e.preventDefault()
     const totalPrice = $('input#price').val()
     const wingCount = $('input#count').val()
     console.log(`Price per wing: ${pricePerWing(totalPrice, wingCount)}`)
-  })
-})
+}
 
 const pricePerWing = (total, count) => {
   return (+total / +count)
